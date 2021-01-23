014262e3e4f022bdd66efcb082d4917df1dd1508
ink8bit <ink8bit@users.noreply.github.com>
2021-01-23 22:09:43 +0300

    Add first implementation

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,25 @@
+mod config;
+
+use config::Config;
+use std::error::Error;
+
+pub fn update(
+    version: &str,
+    changes: &str,
+    user_defined_fields: &str,
+) -> Result<(), Box<dyn Error>> {
+    let config = match Config::new() {
+        Ok(c) => c,
+        Err(e) => panic!("{}", e),
+    };
+    config.update(version, changes, user_defined_fields)?;
+    Ok(())
+}
+
 #[cfg(test)]
 mod tests {
     #[test]
     fn it_works() {
-        assert_eq!(2 + 2, 4);
+        todo!();
     }
 }
