ae8134456b3593f14af46f1467c502a60a55077b
ink8bit <ink8bit@users.noreply.github.com>
2021-02-21 18:38:31 +0300

    Return messages from library to consumer

--- src/lib.rs
+++ src/lib.rs
@@ -9,12 +9,18 @@ use config::{Config, DebyError};
 /// - `version` - an updated version string
 /// - `changes` - changes to be included in your files
 /// - `user_defined_fields` - additional dynamic fields to be included in `control` file
-pub fn update(version: &str, changes: &str, user_defined_fields: &str) -> Result<(), DebyError> {
+pub fn update(
+    version: &str,
+    changes: &str,
+    user_defined_fields: &str,
+) -> Result<(String, String), DebyError> {
     let config = Config::new().map_err(|_| DebyError::ConfigNew)?;
 
-    config
+    let msg = config
         .update(version, changes, user_defined_fields)
         .map_err(|_| DebyError::Update)?;
 
-    Ok(())
+    let (changelog_msg, control_msg) = msg;
+
+    Ok((changelog_msg.to_string(), control_msg.to_string()))
 }
