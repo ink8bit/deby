64af77ac41a11e613d2d78f0aeae39927950a10e
ink8bit <ink8bit@users.noreply.github.com>
2021-02-21 18:28:47 +0300

    Add custom errors

--- src/config/mod.rs
+++ src/config/mod.rs
@@ -1,13 +1,59 @@
 use serde::Deserialize;
 
+use std::fmt;
+use std::fs;
 use std::path::Path;
-use std::{error::Error, fs};
 
 mod changelog;
 mod control;
 
 use changelog::Changelog;
 use control::Control;
+
+#[derive(Debug)]
+pub enum DebyError {
+    ConfigParse,
+    ConfigNew,
+    Update,
+    ConfigRead,
+    Deserialize,
+    DebianDir,
+    ChangelogUpdate,
+    ChangelogOpen,
+    ChangelogRead,
+    ChangelogWrite,
+    ControlUpdate,
+    ControlOpen,
+    ControlWrite,
+}
+
+impl fmt::Display for DebyError {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        match self {
+            DebyError::ConfigNew => {
+                write!(f, "Could not create configuration from config file .debyrc")
+            }
+            DebyError::ConfigParse => write!(f, "Could not parse config file .debyrc"),
+            DebyError::ConfigRead => write!(f, "No config file .debyrc found"),
+
+            DebyError::Update => write!(f, "Could not update your files"),
+            DebyError::Deserialize => write!(f, "Could not deserialize .debyrc config file"),
+            DebyError::DebianDir => write!(
+                f,
+                "No directory with `debian` name found in the root of the project"
+            ),
+
+            DebyError::ChangelogUpdate => write!(f, "Could not update debian changelog file"),
+            DebyError::ChangelogOpen => write!(f, "Could not open debian changelog file"),
+            DebyError::ChangelogRead => write!(f, "Could not read debian changelog file"),
+            DebyError::ChangelogWrite => write!(f, "Could not update debian changelog file"),
+
+            DebyError::ControlUpdate => write!(f, "Could not update debian control file"),
+            DebyError::ControlOpen => write!(f, "Could not open debian control file"),
+            DebyError::ControlWrite => write!(f, "Could not update debian control file"),
+        }
+    }
+}
 
 #[derive(Deserialize, Debug)]
 struct Maintainer {
@@ -26,25 +72,20 @@ pub(crate) struct Config {
 const CONFIG_FILE: &str = ".debyrc";
 
 impl Config {
-    pub(crate) fn new() -> Result<Self, Box<dyn Error>> {
-        let config = Self::parse()?;
+    pub(crate) fn new() -> Result<Self, DebyError> {
+        let config = Self::parse().map_err(|_| DebyError::ConfigParse)?;
+
         Ok(Self {
             changelog: config.changelog,
             control: config.control,
         })
     }
 
-    fn parse() -> Result<Config, Box<dyn Error>> {
-        let config_data = match fs::read_to_string(CONFIG_FILE) {
-            Ok(data) => data,
-            Err(_) => {
-                panic!(
-                    "No config file provided. Create {} file in your project root.",
-                    CONFIG_FILE
-                )
-            }
-        };
-        let config: Config = serde_json::from_str(&config_data)?;
+    fn parse() -> Result<Config, DebyError> {
+        let config_data = fs::read_to_string(CONFIG_FILE).map_err(|_| DebyError::ConfigRead)?;
+        let config: Config =
+            serde_json::from_str(&config_data).map_err(|_| DebyError::Deserialize)?;
+
         Ok(config)
     }
 
@@ -53,20 +94,18 @@ impl Config {
         version: &str,
         changes: &str,
         user_defined_fields: &str,
-    ) -> Result<(), Box<dyn Error>> {
+    ) -> Result<(), DebyError> {
         if !Path::new("debian").exists() {
-            fs::create_dir("debian")?;
+            fs::create_dir("debian").map_err(|_| DebyError::DebianDir)?;
         }
 
-        match Changelog::update(&self, &version, &changes) {
-            Ok(msg) => println!("{}", msg),
-            Err(e) => panic!("{}", e),
-        };
+        let changelog_msg =
+            Changelog::update(&self, &version, &changes).map_err(|_| DebyError::ChangelogUpdate)?;
+        println!("{}", changelog_msg);
 
-        match Control::update(&self, &user_defined_fields) {
-            Ok(msg) => println!("{}", msg),
-            Err(e) => panic!("{}", e),
-        };
+        let control_msg =
+            Control::update(&self, &user_defined_fields).map_err(|_| DebyError::ControlUpdate)?;
+        println!("{}", control_msg);
 
         Ok(())
     }
